a422c8d22211c6436a1f4215d821837b50693bad
mastertth <tsukasa.japan1201@gmail.com>
2019-10-04 00:20:47 +0900

    implement thres image

---
+++ Assets/Scripts/Effect/BaseThres.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BaseThres : MonoBehaviour
+{
+    const string SHADER_NAME = "Custom/BaseThres";
+
+    [Range(0, 1)]
+    public float thres = 1;
+
+    int idThres = -1;
+
+    [SerializeField]
+    private Shader _shader;
+    public Shader shader
+    {
+        get
+        {
+            if (this._shader == null)
+            {
+                this._shader = Shader.Find(SHADER_NAME);
+            }
+            return this._shader;
+        }
+    }
+
+    private Material _material;
+    private Material material
+    {
+        get
+        {
+            if (this._material == null)
+            {
+                this._material = new Material(shader);
+            }
+            return this._material;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (this._material != null)
+        {
+            DestroyImmediate(this._material);
+        }
+        this._material = null;
+    }
+
+    private void Awake()
+    {
+        this.idThres = Shader.PropertyToID("_Thres");
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        material.SetFloat(this.idThres, this.thres);
+        Graphics.Blit(src, dest, material);
+    }
+}
---
+++ Assets/Shader/BaseThres.shader
@@ -0,0 +1,35 @@
+﻿Shader "Custom/BaseThres"
+{
+    Properties
+    {
+        _MainTex("Texture", 2D) = ""{}
+        _Thres("Thres", Range(0, 1)) = 0.5
+    }
+
+    SubShader
+    {
+        pass 
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+
+            sampler2D _MainTex;
+            half _Thres;
+
+            fixed4 frag(v2f_img i) : COLOR 
+            {
+                fixed4 c = tex2D(_MainTex, i.uv);
+                float gray = c.r * 0.3 + c.g * 0.6 + c.b * 0.1;
+                float val = 0;
+                if (_Thres < gray) {
+                    val = 1;
+                }
+                return fixed4(val, val, val, 1);
+            }
+            ENDCG
+        }
+    }
+
+}
